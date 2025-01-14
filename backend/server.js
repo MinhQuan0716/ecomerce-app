ee32f069aa68e91bac793399c733013ac5e5cdfe
MinhQuan0716 <minhquantrantrinh731@gmail.com>
2025-01-14 14:32:02 +0700

    fix server.js cors issue

--- backend/server.js
+++ backend/server.js
@@ -10,25 +10,12 @@ import orderRouter from './routes/orderRoute.js';
 //App Config
 const app = express();
 const port= process.env.PORT || 4000;
-const allowedOrigins = [
-    process.env.FRONTEND_URL || 'http://localhost:5173', // Frontend client
-    process.env.ADMIN_URL || 'http://localhost:5174'    // Admin client
-  ];
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  };
+
 connectDB();
 connectCloudinary();
 //Middlewares
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/api/user',userRouter);
 app.use('/api/product',productRouter);
 app.use('/api/cart',cartRouter);
